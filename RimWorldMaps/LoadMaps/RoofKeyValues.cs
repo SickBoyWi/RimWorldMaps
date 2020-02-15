9dce59ca7560169e4aedc6f34d267884113eb2c8
Johnny Hastings <sick.boy.wi@gmail.com>
2020-02-15 08:32:18 -0600

    Initial Upload.

---
+++ RimWorldMaps/LoadMaps/RoofKeyValues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Verse;
+
+namespace RimWorldMaps
+{
+    [StaticConstructorOnStartup]
+    static class RoofKeyValues
+    {
+        public static Dictionary<ushort, string> ROOF_KEY_VALUE_USHORT = new Dictionary<ushort, string>();
+
+        static RoofKeyValues ()
+        {
+            ROOF_KEY_VALUE_USHORT.Add((ushort)5133, "RoofConstructed");
+            ROOF_KEY_VALUE_USHORT.Add((ushort)10820, "RoofRockThick");
+            ROOF_KEY_VALUE_USHORT.Add((ushort)6699, "RoofRockThin");
+        }
+    }
+}
---
+++ RimWorldMaps/LoadMaps/ThingHolder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Verse;
+
+namespace RimWorldMaps
+{
+    class ThingHolder
+    {
+        public string defName;
+        public int rot;
+        public IntVec3 pos;
+        public int count;
+        public string kind;
+        public string stuff;
+        // TODO: THIS IS UNUSED AS YET. Needed for insects or mechs.
+        //public string faction;
+
+        public ThingHolder(string defNameIn, int rotIn, IntVec3 posIn, int countIn, string stuffIn, string kindIn)
+        {
+            defName = defNameIn;
+            rot = rotIn;
+            pos = posIn;
+            count = countIn;
+            stuff = stuffIn;
+            kind = kindIn;
+        }
+    }
+}
